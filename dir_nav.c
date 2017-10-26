99736ba3445397c1888b3f38931e1db715c0f83d
brandoneprice31 <bprice@college.harvard.edu>
2017-10-26 12:54:27 -0400

    replaces tilda's with provided name

--- dir_nav.c
+++ dir_nav.c
@@ -28,6 +28,10 @@ void copy_everything(char* src_dir_name, char* dest_dir_name) {
                 continue;
             if (!strcmp (in_file->d_name, ".."))
                 continue;
+            if (!strcmp (in_file->d_name, "venv"))
+                continue;
+                if (!strcmp (in_file->d_name, "__pycache__"))
+                    continue;
 
             // mkdir
             mkdir(dest_file_path, 0700);
@@ -43,12 +47,24 @@ void copy_everything(char* src_dir_name, char* dest_dir_name) {
             FILE* out_file = fopen(dest_file_path, "w");
 
             // Copy in file into out file.
+            size_t bytes_in = 0;
             size_t sz_read;
             while ((sz_read = fread(BUFFER, 1, 256, curr_file))) {
+
+                // mark the ~
+                for (int i=0; i<= (int) sz_read; i++) {
+                    if (BUFFER[i] == '~') {
+                        memcpy(tilda_marker_arr[tilda_marker_pos].filepath, dest_file_path, strlen(dest_file_path));
+                        tilda_marker_arr[tilda_marker_pos].tilda_pos = bytes_in + i;
+                        tilda_marker_pos++;
+                    }
+                }
+
                 fwrite(BUFFER, 1, sz_read, out_file);
+                bytes_in+=sz_read;
             }
 
-            fclose(out_file);
+            fclose(curr_file);
             fclose(out_file);
         }
     }
--- dir_nav.h
+++ dir_nav.h
@@ -1,3 +1,11 @@
+struct tilda_marker {
+    char filepath[1024];
+    size_t tilda_pos;
+};
+
+int tilda_marker_pos;
+struct tilda_marker tilda_marker_arr[1024];
+
 char BUFFER[BUFSIZ];
 
 void copy_everything(char* src_dir_name, char* dest_dir_name);
